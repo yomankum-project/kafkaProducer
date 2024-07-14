0908d7705717a3daa3d898b28a206b3a6fcbaccc
h0l1da2 <whaleee@naver.com>
2024-07-14 09:35:26 +0900

    feat(kafka): 카프카 설정
    기본 설정
    
    holiday

---
+++ src/main/java/yomankum/kafka/producer/config/KafkaConfig.java
@@ -0,0 +1,32 @@
+package yomankum.kafka.producer.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, Object> factory() {
+        Map<String, Object> props = new HashMap<>();
+        // 컨테이너를 사용했기 때문에 카프카 컨테이너 이름으로 설정
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> template() {
+        return new KafkaTemplate<>(factory());
+    }
+}
